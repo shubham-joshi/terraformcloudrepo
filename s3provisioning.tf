3602dd86bccacc19c2547f95fce6763abe982512
Shubham <contact@shubhamjoshi.co.in>
2020-09-12 11:30:06 +0530

    Initial s3 provisioning code

---
+++ s3provisioning.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+    # access_key = ""
+    # secret_key = ""
+    region = "ap-south-1"
+}
+
+resource "aws_s3_bucket" "cvwebsite" {
+    bucket = "web.shubhamjoshi.co.in"
+    acl = "public-read"
+
+    website {
+        index_document = "index.html"
+        error_document = "404.html"
+    }
+}
